528660d8d05c809c0d14bd406ecb742f646db349
Djordje <dimovicdj@gmail.com>
2018-05-11 05:34:55 +0200

    add functionality to buttons and search from address bar

--- main.py
+++ main.py
@@ -5,25 +5,21 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebKit import *
 from PyQt5.QtWebKitWidgets import * 
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.setGeometry(50, 50, 1000, 800)
+        self.setGeometry(10, 10, 1000, 800)
+        
         self.setWindowTitle('Browser')
         self.setWindowIcon(QIcon('web.png'))
         
-        self.main_bar = MainBar(self) 
-        self.web = QWebView()
-        self.web.load(QUrl('https://www.google.com'))
-
-        _widget = QWidget() 
-        _layout = QVBoxLayout(_widget)
-        #add widgets here
-        _layout.addWidget(self.main_bar)
-        _layout.addWidget(self.web)
-        _layout.setAlignment(Qt.AlignTop)
-
-        self.setCentralWidget(_widget)
+        self.createBrowser()
+        
+    def createBrowser(self):
+        self.browser = Browser(self)
+        self.setCentralWidget(self.browser)
+        
         
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Confirm close',
@@ -34,18 +30,81 @@ class MainWindow(QMainWindow):
             event.accept()
         else:
             event.ignore() 
+            
 
-class MainBar(QWidget):
+class Browser(QWidget):
 
     def __init__(self, parent):
-        super(MainBar, self).__init__(parent)
+        super(Browser, self).__init__(parent)
+        
+        _layout = QVBoxLayout(self)
+        
         self.__controls()
         self.__layout()
+        _layout.addLayout(self.hbox)
+        
+        self.homePage = QUrl('https://www.google.com')
+        self.web = QWebView()
+        self.web.load(self.homePage)
+        self.web.setMinimumHeight(qApp.primaryScreen().size().height()-160)
+        _layout.addWidget(self.web)
+        
+        self.searchHistory = self.web.page().history()
+        
+        _layout.setAlignment(Qt.AlignTop)
 
+    def historyButtonPush(self):
+        self.popUp = QListWidget()
+        
+        for h in self.searchHistory.items():
+            self.popUp.addItem(QListWidgetItem(h.title()))
+            
+        self.popUp.setWindowTitle('History')
+        self.popUp.setMinimumSize(400, 400)
+        
+        self.popUp.itemActivated.connect(self.doubleClick)
+        self.popUp.show()
+        
+        
+    def doubleClick(self, item):
+        for i in range(0, self.searchHistory.count()):
+            if(self.searchHistory.itemAt(i).title() == item.text()):
+                self.web.load(self.searchHistory.itemAt(i).originalUrl())
+                break
+        
+
+    def forwardButtonPush(self):
+        self.searchHistory.forward()
+        
+    def backButtonPush(self):
+        self.searchHistory.back()
+
+    def refreshButtonPush(self):
+        self.web.reload()
+
+    def homeButtonPush(self):
+        self.web.load(self.homePage)
 
     def goButtonPush(self):
-        #TODO 
-        asd = 5
+        tmp = self.addressBar.text()
+        
+        if(tmp == ''):
+            return
+        
+        q = QUrl(tmp)
+        if(q.scheme() == ""):
+            q.setScheme("http");
+            
+        if(tmp.find('.') != -1 and tmp.find(' ') == -1):
+            self.web.load(q)
+                
+        else:
+            l = tmp.split()
+            link = 'https://www.google.com/search?q=' + l[0]
+            for i in range(1, len(l)):
+                link += '+' + l[i]
+            self.web.load(QUrl(link))
+        
     def __controls(self):
         self.back = QPushButton('back')
         self.forward = QPushButton('forward')
@@ -57,27 +116,34 @@ class MainBar(QWidget):
         self.addressBar = QLineEdit()
         self.go = QPushButton('go')
         
+        self.history.clicked.connect(self.historyButtonPush)
+        self.forward.clicked.connect(self.forwardButtonPush)
+        self.back.clicked.connect(self.backButtonPush)
+        self.refresh.clicked.connect(self.refreshButtonPush)
+        self.addressBar.editingFinished.connect(self.goButtonPush)
         self.go.clicked.connect(self.goButtonPush)
+        self.home.clicked.connect(self.homeButtonPush)
 
     def __layout(self):
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.back)
-        hbox.addWidget(self.forward)
-        hbox.addWidget(self.refresh)
-        hbox.addWidget(self.home)
-        hbox.addWidget(self.addressBar)   
-        hbox.addWidget(self.go)
-        hbox.addWidget(self.bookmarks)
-        hbox.addWidget(self.history)
-        hbox.addWidget(self.settings)                
-        self.setLayout(hbox)
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(self.back)
+        self.hbox.addWidget(self.forward)
+        self.hbox.addWidget(self.refresh)
+        self.hbox.addWidget(self.home)
+        self.hbox.addWidget(self.addressBar)   
+        self.hbox.addWidget(self.go)
+        self.hbox.addWidget(self.bookmarks)
+        self.hbox.addWidget(self.history)
+        self.hbox.addWidget(self.settings) 
+        
 
+          
 
 def main():
     app = QApplication(sys.argv)
 
     win = MainWindow()
-    win.show()
+    win.showMaximized()
     app.exec_()
 
 if __name__ == '__main__':
